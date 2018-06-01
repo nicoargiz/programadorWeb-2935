4266f974582958022e4810b66f4fa9d215a0202f
Nico <nicoargiz@icloud.com>
2018-05-31 21:30:52 -0300

    subiendo algo

---
+++ Clase 02/Alumno/Ejercicio/index.js
@@ -0,0 +1,17 @@
+var dia = prompt('Decime un día de la semana')
+
+switch (dia) {
+  case 'lunes':
+  case 'martes':
+  case 'miercoles':
+  case 'jueves':
+  case 'viernes':
+    console.log(dia + ' es un día de semana')
+    break
+  case 'sabado':
+  case 'domingo':
+    console.log(dia + ' es un dia de joda')
+    break
+  default:
+    console.log('sos un gil')
+}
--- Clase 02/Profe/En clase/js/index.js
+++ Clase 02/Profe/En clase/js/index.js
@@ -1,10 +1,37 @@
 // 1. Condicionales
 // - ìf
+/*
+var name = prompt('Ingresa tu nombre')
+if (name) {
+  console.log('Hola ' + name)
+} else {
+  name = prompt('Esta vez escribilo')
+}
+
 // - switch
 // 2. Operadores de comparación
+
 // - ===
-// - !==
-// - <
+var result = prompt('Ingrese resultado', 'Ganó, Empató o Perdió')
+
+if ('Ganó' === result) {
+  console.log('Sumaste 3 puntos')
+} else {
+  if ('Empató' === result) {
+    console.log('Sumaste 1 punto')
+  } else {
+    if ('Perdió' === result) {
+      console.log('Sumaste 0 puntos')
+    } else {
+      console.log('Resultado incorrecto')
+    }
+  }
+}
+*/
+
+// - !== (Estricto desigual, exactamente lo mismo que el === pero al reves)
+
+// - < 
 // - >
 // - >=
 // - <=
