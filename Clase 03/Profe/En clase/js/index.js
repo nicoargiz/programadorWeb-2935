ee4ff14ddfebe8b621c8dc53aa54f41be450b970
Nico <nicoargiz@icloud.com>
2018-06-05 20:47:45 -0300

    te odio git

--- Clase 03/Profe/En clase/js/index.js
+++ Clase 03/Profe/En clase/js/index.js
@@ -8,10 +8,51 @@
 
 // # Bucles e iteración
 
+console.log('Init app')
+
 // 1. Sentencias de blucle
 //  - while
+
+// var counter = 1
+
+// while (counter <= 6) {
+//   console.log(counter)
+//   counter = counter + 1
+// }
+
+// var name = prompt('Ingrese su nombre')
+
+// while (!name) {
+//   name = prompt('Ingrese su nombre')
+// }
+
+// console.log('Hola ' + name)
+
 //  - do while
+
+// var name
+
+// do {
+//   name = prompt('Ingrese su nombre')
+// } while (!name)
+
+// console.log('Hola ' + name)
+
 //  - for
+
+// for (var i = 0; i < 3; i++) {
+//   console.log(i)
+// }
+
+var students = ['Adrian', 'Javier', 'Alicia', 'Pepe']
+var = student
+
+for (var i = 0; i < students.length; i++) {
+  student = students[i]
+  console.log(student + ' es un alumno')
+}
+
+
 // 2. Sentencias de corte
 //  - break
 //  - continue
