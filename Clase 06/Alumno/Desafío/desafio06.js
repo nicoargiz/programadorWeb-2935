6df0d0314498373651bd3d6e21b46a6bda333ee0
Nico <nicoargiz@icloud.com>
2018-06-17 21:38:38 -0300

    Subiendo desafio 06

---
+++ Clase 06/Alumno/Desafío/desafio06.js
@@ -0,0 +1,26 @@
+console.log('Hola Tero D.06')
+
+// includesText('Pa', 'Patricia') Deberá devolver true
+
+// includesText('Patricia', 'Pa') Deberá devolver false
+
+// includesText(2, 'Pa') Deberá devolver false
+
+function busqueda (buscaEsto, enEsto) {
+  if (typeof buscaEsto === 'string' && typeof enEsto === 'string') {
+    buscaEstoMayus = buscaEsto.toUpperCase()
+    enEstoMayus = enEsto.toUpperCase()
+
+    if (enEstoMayus.indexOf(buscaEstoMayus) >= 0) {
+      return true
+    } else {
+      return false
+    }
+  } else {
+    return false
+  }
+}
+
+console.log(busqueda('Pa', 'Patricia'))
+console.log(busqueda('Patricia', 'Pa'))
+console.log(busqueda(2, 'Pa'))
--- Clase 06/Alumno/Ejercicio/ejercicio06.js
+++ Clase 06/Alumno/Ejercicio/ejercicio06.js
@@ -1,10 +1,10 @@
 var numbers = [ 1, 2, 3, 4, 5 ]
-var noArray = 'Anda a cagar'
+var noArray = 'No soy un array'
 
 // deleteElement(2, numbers)  Devuelve un nuevo Array [1,2,4,5]
 
 function deleteElement (index, hola) {
-  if (Array.isArray(hola)) {
+  if (Array.isArray(hola) && typeof index === 'number') {
     var hola1 = hola.slice()
     hola1.splice(index, 1)
     return hola1
